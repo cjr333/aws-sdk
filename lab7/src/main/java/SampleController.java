1f03044b7acdd2e7c2e64166cecf018d4d262c06
cjr333 <cjr_333@naver.com>
2017-06-14 13:50:29 +0900

    lab7 실습

---
+++ lab7/src/main/java/SampleController.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
